ed5d86723a69f2a7167634a64940d9f9656168d5
montlikadani <montlikada@gmail.com>
2023-02-22 09:53:04 +0100

    half done

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace BasicPuzzle {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.draggablePanel = new System.Windows.Forms.Panel();
+            this.resultPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // draggablePanel
+            // 
+            this.draggablePanel.AutoSize = true;
+            this.draggablePanel.Location = new System.Drawing.Point(13, 13);
+            this.draggablePanel.Name = "draggablePanel";
+            this.draggablePanel.Size = new System.Drawing.Size(200, 100);
+            this.draggablePanel.TabIndex = 0;
+            // 
+            // resultPanel
+            // 
+            this.resultPanel.AllowDrop = true;
+            this.resultPanel.AutoSize = true;
+            this.resultPanel.Location = new System.Drawing.Point(417, 13);
+            this.resultPanel.Name = "resultPanel";
+            this.resultPanel.Size = new System.Drawing.Size(353, 327);
+            this.resultPanel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(414, 365);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Húzza a képeket ebbe a felső dobozba";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultPanel);
+            this.Controls.Add(this.draggablePanel);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Puzzle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel draggablePanel;
+        private System.Windows.Forms.Panel resultPanel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BasicPuzzle {
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+
+            int size = 80;
+
+            Control.ControlCollection draggableControls = draggablePanel.Controls;
+            Control.ControlCollection resultControls = resultPanel.Controls;
+
+            for (int i = 0; i < 4; i++) {
+                for (int a = 0; a < 4; a++) {
+                    PictureBox pictureBox = new PictureBox() {
+                        BackgroundImage = Image.FromFile(GetResourceFileByName($"mountains2-{i}-{a}.png")),
+                        BackgroundImageLayout = ImageLayout.Stretch,
+                        Bounds = new Rectangle(a * size, i * size, size, size),
+                        Cursor = Cursors.Hand
+                    };
+
+                    PictureBox emptyBox = new PictureBox() {
+                        BackgroundImageLayout = ImageLayout.Stretch,
+                        Bounds = pictureBox.Bounds,
+                        BorderStyle = BorderStyle.FixedSingle,
+                        AllowDrop = true,
+                        Tag = pictureBox
+                    };
+
+                    pictureBox.MouseDown += MouseDownEvent;
+                    emptyBox.DragDrop += DragDropEvent;
+                    emptyBox.DragEnter += DragEnterEvent;
+
+                    draggableControls.Add(pictureBox);
+                    resultControls.Add(emptyBox);
+                }
+            }
+        }
+
+        private void MouseDownEvent(object sender, MouseEventArgs e) {
+            if (sender is PictureBox pict) {
+                pict.Hide();
+
+                if (pict.DoDragDrop(pict.BackgroundImage, DragDropEffects.All) == DragDropEffects.None) {
+                    pict.Show();
+                }
+            }
+        }
+
+        private void DragDropEvent(object sender, DragEventArgs e) {
+            if (sender is PictureBox pict) {
+                pict.BackgroundImage = (Image)e.Data.GetData(DataFormats.Bitmap);
+                pict.BorderStyle = BorderStyle.None;
+
+                if (pict.Tag is PictureBox sec) {
+                    sec.Hide();
+                    sec.Tag = null;
+                    pict.Tag = sec;
+                }
+            }
+        }
+
+        private void DragEnterEvent(object sender, DragEventArgs e) {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private string GetResourceFileByName(string name) {
+            return string.Format("{0}Resources\\{1}", Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\")), name);
+        }
+    }
+}
